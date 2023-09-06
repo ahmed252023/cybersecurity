df32cbc494bd638674aefb21e5379da4a385b1dd
ahmed252023 <131555266+ahmed252023@users.noreply.github.com>
2023-09-06 07:26:54 -0400

    Update EmailModel.java

--- dns-test/src/main/java/tn/itbs/sb/model/EmailModel.java
+++ dns-test/src/main/java/tn/itbs/sb/model/EmailModel.java
@@ -5,42 +5,4 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class EmailModel {
 	
-//
-//	public class HunterResponse {
-//	    private HunterData data;
-//
-//	    public HunterData getData() {
-//	        return data;
-//	    }
-//
-//	    public void setData(HunterData data) {
-//	        this.data = data;
-//	    }
-//	}
-//
-//	public class HunterData {
-//	    private List<HunterEmail> emails;
-//
-//	    public List<HunterEmail> getEmails() {
-//	        return emails;
-//	    }
-//
-//	    public void setEmails(List<HunterEmail> emails) {
-//	        this.emails = emails;
-//	    }
-//	}
-//
-//	public class HunterEmail {
-//	    private String value;
-//
-//	    public String getValue() {
-//	        return value;
-//	    }
-//
-//	    public void setValue(String value) {
-//	        this.value = value;
-//	    }
-//	}
-
-
 }
